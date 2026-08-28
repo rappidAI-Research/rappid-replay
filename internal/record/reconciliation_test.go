3f70098892b2dcd70ac9e25eb096743997aedefb
Jonas Cikemgil <jonascikemgil07@gmail.com>
2026-08-28 11:12:11 +0200

    style: gofmt reconciliation integration test

--- internal/record/reconciliation_test.go
+++ internal/record/reconciliation_test.go
@@ -34,7 +34,7 @@ func TestGenericRecorderPublishesWatcherTriggeredCheckpoint(t *testing.T) {
 		t.Fatal(err)
 	}
 	result, err := Run(ctx, Dependencies{DB: db, CAS: cas}, Options{
-		Command: []string{os.Args[0], "-test.run=^TestReplayReconciliationHelperProcess$", "--", workspace},
+		Command:    []string{os.Args[0], "-test.run=^TestReplayReconciliationHelperProcess$", "--", workspace},
 		WorkingDir: workspace,
 		Env: append(os.Environ(),
 			"RAPPID_REPLAY_RECONCILE_HELPER=1",
